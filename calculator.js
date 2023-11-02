d350ad23fe31a7cd35120da090d8ab6da536ff2e
Michael Meola <meolaadonai@gmail.com>
2023-11-02 15:46:37 -0600

    almost done

--- calculator.js
+++ calculator.js
@@ -1,4 +1,79 @@
-function calculate(expression) {}
+function add(num1, num2){
+  return num1 + num2
+}
+function subtract(num1, num2){
+  return num1 - num2
+}
+function multiply(num1, num2){
+  return num1 * num2
+}
+function divide(num1, num2){
+  return num1 / num2
+}
+function power(num1, num2){
+  return num1 ** num2
+}
+function mod(num1, num2){
+  return num1 % num2
+}
+function sqrt(num){
+  return num ** 0.5
+}
+
+function calculate(expression){
+
+  let num1
+  let operator
+  let num2
+
+  const tokens = expression.split(` `)
+
+  if(tokens.length === 3){
+    num1 = Number(tokens[0])
+    operator = tokens[1]
+    num2 = Number(tokens[2])
+    if(Number.isNaN(num1) || Number.isNaN(num2)){
+      alert('Enter a valid number!')
+      return
+    }
+  }
+  if(tokens.length === 2){
+    operator = tokens[0]
+    num1 = Number(tokens[1])
+    if(Number.isNaN(num1)){
+      alert('Enter a valid number!')
+      return
+      }
+    }
+
+    if(operator === '+'){
+    return add(num1, num2)
+    }
+  
+    if(operator === '-'){
+      return subtract(num1, num2)
+    }
+    if(operator === '*'){
+      return multiply(num1, num2)
+    }
+  
+    if(operator === '/'){
+      return divide(num1, num2)
+    }
+  
+    if(operator === '^'){
+      return power(num1, num2)
+    }
+  
+    if(operator === '%'){
+      return mod(num1, num2)
+    }
+
+    if (operator === 'sqrt') {
+      return sqrt(num1);
+    } 
+  }
+
 
 /* **************** DO NOT EDIT THE CODE BELOW **************** */
 /* ************************************************************ */
